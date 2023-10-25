9a2e14f9be3a31ff5b74931e0460de90ae2bf325
Georges <georges.hinot@gmail.com>
2023-10-25 20:14:18 +0200

    Standard update

--- test.py
+++ test.py
@@ -1,45 +1,85 @@
+"""
+API WIKIPEDIA COMPLET :
+- On aide la recherche
+- On teste son résultat
+- On récupère les homonymes selon Wikipédia
+- On lance la recherche sur les homonymes
+- On teste les résultats
+"""
 
-########################### Api Wikipedia exemple choix testé
 import wikipedia
-
+import requests
 import warnings
 warnings.catch_warnings()
 warnings.simplefilter('ignore')
 
 wikipedia.set_lang('fr')
 
-chercher = 'aiguille dibona'
+# On précise ici la chaîne à chercher car on l'utilisera sans doute plusieurs fois
+chercherMot = 'meije'
+
+# Chaîne pour aider à trouver l'article
+aide = 'montagne'
+
+# Chaîne pour vérification du résultat
+# On la précise ici car on l'utilisera sans doute plusieurs fois
 chaineTemoin = 'Massif des Écrins'
-numberPhrase = 2
 
-myWikiContent = ''
+# Nombre de phrases à récupérer
+# On le précise ici car on l'utilisera sans doute plusieurs fois
+numberPhrase = 3
 
-print('\nChercher :', chercher)
-try:
-    myWikiContent = wikipedia.summary(chercher, sentences=numberPhrase)
+myWikiContent = 'Pas pertinent 0 !'
 
-    # Vérifier la pertinence du résultat
-    if str(chaineTemoin).lower() in str(myWikiContent).lower():
-        pass
+if chercherMot != '' and chercherMot != None :
 
-    else:
-        myWikiContent = 'Pas pertinent !!!'
+    chercher = aide + ' ' + chercherMot
 
-# Gestion des DisambiguationErrors
-except wikipedia.DisambiguationError as e:
-    # print('\nAttention : 1ère DisambiguationError!')
-    for p in e.options:
-        # print('Option testée : ' + p)
-        try:
-            myWikiContent = wikipedia.summary(p, sentences=numberPhrase)
-            # Vérifier la pertinence du résultat
-            if str(chaineTemoin).lower() in str(myWikiContent).lower():
-                pass
-            if not str(chaineTemoin).lower() in str(myWikiContent).lower():
-                myWikiContent = 'Pas pertinent !!!'
+    print('\nChercher :', chercher)
+    try:
+        myWikiContent = wikipedia.summary(chercher, sentences=numberPhrase)
 
-        except wikipedia.DisambiguationError as e:
-            # print('\nAttention : 2ème DisambiguationError!')
-            myWikiContent = 'Pas pertinent !!!'
+        # Vérifier la pertinence du résultat
+        if str(chaineTemoin).lower() in str(myWikiContent).lower():
+            pass
 
-print(str(myWikiContent))
+        else:
+            myWikiContent = 'Pas pertinent 1 !'
+
+    except wikipedia.exceptions.PageError as e:
+        myWikiContent = 'Pas trouvé 1 !'
+
+    except requests.exceptions.ConnectionError as e:
+        myWikiContent = 'Pas trouvé 3 !'
+
+    # Gestion des DisambiguationErrors
+    except wikipedia.DisambiguationError as e:
+        for p in e.options:
+            # print('Option testée : ' + p)
+            try:
+                myWikiContent = wikipedia.summary(p, sentences=numberPhrase)
+                # Vérifier la pertinence du résultat
+                if str(chaineTemoin).lower() in str(myWikiContent).lower():
+                    pass
+                if not str(chaineTemoin).lower() in str(myWikiContent).lower():
+                    myWikiContent = 'Pas pertinent 2 !'
+
+            except wikipedia.DisambiguationError as e:
+                myWikiContent = 'Pas pertinent 3 !'
+
+            except wikipedia.exceptions.PageError as e:
+                myWikiContent = 'Pas trouvé 2 !'
+
+            except requests.exceptions.ConnectionError as e:
+                myWikiContent = 'Pas trouvé 4 !'
+
+    import re
+
+    myWikiContent = myWikiContent.split('==')[0]
+
+    myWikiContent = myWikiContent.replace('\n\n', '')
+
+    myWikiContent = myWikiContent.replace('. ', '\n')
+
+    print('\n' + str(myWikiContent))
+    print('fin')
